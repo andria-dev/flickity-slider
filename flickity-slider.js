270d99f40ba3a10a5e64d8d13cb9eb4d5a876abe
Christopher Brown <chbphone55@gmail.com>
2018-11-05 00:07:00 -0700

    convert to lit-element

--- flickity-slider.js
+++ flickity-slider.js
@@ -1,4 +1,4 @@
-import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import {LitElement, html} from '@polymer/lit-element';
 
 /**
  * `flickity-slider`
@@ -8,15 +8,15 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
  * @polymer
  * @demo demo/index.html
  */
-class FlickitySlider extends PolymerElement {
-  static get template() {
+class FlickitySlider extends LitElement {
+  render() {
     return html`
       <style>
         :host {
           display: block;
         }
       </style>
-      <h2>Hello [[prop1]]!</h2>
+      <h2>Hello ${this.prop1}!</h2>
     `;
   }
   static get properties() {
