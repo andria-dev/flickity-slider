2fb5cd0a7dbd4c71163914f368757b188a36572a
Christopher Brown <chbphone55@gmail.com>
2018-11-07 09:30:06 -0700

    try test-fixture

--- test/flickity-slider.test.js
+++ test/flickity-slider.test.js
@@ -1,8 +1,20 @@
-describe('flickity-tester', () => {
-  console.log('suite up')
-  describe('please work', () => {
-    it('pllleeeasseee', () => {
-      expect([]).to.be.empty
+describe('<flickity-slider>', () => {
+  let el
+
+  describe('Regsitration', () => {
+    beforeEach(() => {
+      el = fixture('simple')
+      debugger
+    })
+
+    it('is an element', () => {
+      expect(el).to.not.be.equal(undefined)
+      expect(el instanceof HTMLElement).to.be.true
+    })
+
+    it('flickity is created', () => {
+      expect(el._flickity).to.not.be.equal(undefined)
+      expect(el._flickity instanceof Flickity).to.be.true
     })
   })
 })
