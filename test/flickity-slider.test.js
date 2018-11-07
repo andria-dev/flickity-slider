c9d5eb52e74221d77e086150800ad3b482c00502
Christopher Brown <chbphone55@gmail.com>
2018-11-07 10:56:58 -0700

    More tests

--- test/flickity-slider.test.js
+++ test/flickity-slider.test.js
@@ -7,17 +7,51 @@ describe('<flickity-slider>', () => {
     })
 
     it('is an element', () => {
-      expect(el).to.not.be.equal(undefined)
-      expect(el instanceof HTMLElement).to.be.true
+      expect(el).to.be.instanceof(HTMLElement, 'el is not an instance of HTMLElement')
     })
 
     it('has an instance of Flickity', () => {
-      expect(el._flickity).to.not.be.equal(undefined)
-      expect(el._flickity instanceof Flickity).to.be.true
+      expect(el._flickity).to.be.instanceof(Flickity, 'el._flickity is not an instance of Flickity')
+    })
+
+    it('has default options', () => {
+      expect(el.options).to.deep.equal({
+        prevNextButtons: true,
+        pageDots: true
+      }, 'default options are incorrect')
+    })
+
+    it('has default parallax', () => {
+      expect(el.parallax).to.be.equal(false, 'parallax should default to false')
+    })
+
+    it('has Flickity generated elements', () => {
+      expect(el.children[0].className).to.be.equal('flickity-viewport')
+      expect(el.children[0].children[0].className).to.be.equal('flickity-slider')
+
+      expect(el.children[1].tagName).to.be.equal('BUTTON')
+      expect(el.children[2].tagName).to.be.equal('BUTTON')
+      expect(el.children[3].tagName).to.be.equal('OL')
+
+      expect(el.children[1].className).to.be.equal('flickity-button flickity-prev-next-button previous')
+      expect(el.children[2].className).to.be.equal('flickity-button flickity-prev-next-button next')
+      expect(el.children[3].className).to.be.equal('flickity-page-dots')
     })
   })
 
   describe('Slotted children', () => {
-    
+    beforeEach(() => {
+      el = fixture('slotted-children')
+    })
+
+    it('has correctly slotted children', () => {
+      expect(el.children[0].children[0].children).to.have.lengthOf(4, 'number of slotted children is not 4')
+    })
+  })
+
+  describe('Parallax', () => {
+    beforeEach(() => {
+      el = fixture('parallax')
+    })
   })
 })
